7d1b49a815d89952c6e29559f6b09ed91bee35ee
Niteesh <niteeshravi343@gmail.com>
2026-05-26 18:32:26 +0530

    run build in project

--- booksproject/src/main.jsx
+++ booksproject/src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles/style.css";
+import "./Styles/Style.css";
 
 ReactDOM.createRoot(
   document.getElementById("root")
